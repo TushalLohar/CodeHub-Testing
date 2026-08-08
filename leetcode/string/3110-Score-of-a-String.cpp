97d5c11740a3797ff858936c8010e19e91e09189
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:22:48 +0530

    Add Score-of-a-String (string)

---
+++ leetcode/string/3110-Score-of-a-String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int i =0;
+        int sum =0;
+        while(i<s.size()-1){
+            sum = sum + abs(s[i]-s[i+1]);
+            i++;
+        }
+        return sum;
+        
+    }
+};
