85199ce07d55fd702da504bd617a01d138bc50ab
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:38:50 +0530

    Add Minimise Sum (1000)

---
+++ codeforces/1000/2124B - Minimise Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0; i<n; i++) cin >> arr[i];
+        cout << min(arr[0], arr[1]) + arr[0] << endl;
+    }
+}
