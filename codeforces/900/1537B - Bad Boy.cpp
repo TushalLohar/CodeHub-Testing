504c45e199fa945a25caa411d545d21edcd2b3fb
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:28:23 +0530

    Add Bad Boy (900)

---
+++ codeforces/900/1537B - Bad Boy.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--) {
+
+        ll n,m,i,j;
+        cin>>n>>m>>i>>j;
+
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+
+    }
+
+    return 0;
+}
