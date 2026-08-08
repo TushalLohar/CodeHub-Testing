5a743465f6b4b1af237cee74a80f91c226b76bf3
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:09:21 +0530

    Add RemovevomeR (1000)

---
+++ codeforces/1000/2241C - RemovevomeR.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int ans = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] != s[i - 1])
+            ans++;
+    }
+
+    int mini = (ans == 2 ? 2 : 1);
+    cout << mini <<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
