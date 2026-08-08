e89b4d70584c339ff9e4461bb1568dd5aa80ec5f
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:29:27 +0530

    Add Antimedian Deletion (800)

---
+++ codeforces/800/2211A - Antimedian Deletion.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n;
+        cin >> n;
+
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++) cin >> nums[i];
+
+        if (n == 1) {
+            cout << 1 << endl;
+        } else {
+            for (int i = 0; i < n; i++) {
+                cout << 2 << " ";
+            }
+            cout << endl;
+        }
+
+    }
+
+    return 0;
+}
