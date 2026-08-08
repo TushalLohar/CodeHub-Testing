d5d92cb8e50458925972ed20dba485350dd40c38
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 19:07:18 +0530

    Add Palindrome, Twelve and Two Terms (800)

---
+++ codeforces/800/2234B - Palindrome, Twelve and Two Terms.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    ll n;
+    cin>>n;
+        vector<ll>nums={0,1,2,3,4,5,6,7,8,9,22,11};
+
+    if(n==10){
+        cout<<-1<<endl;
+        return;
+    }
+
+    ll a=nums[n%12];
+    ll b = n-a;
+
+    cout<<a<<" "<<b<<endl;
+
+
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t=1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
