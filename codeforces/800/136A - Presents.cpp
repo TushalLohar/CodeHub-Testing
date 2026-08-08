0327c08d1ccda7fade860a504d9412a513f0503f
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:38:34 +0530

    Add Presents (800)

---
+++ codeforces/800/136A - Presents.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    unordered_map<int,int> mp;
+ 
+    for(int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        mp[x] = i;
+    }
+ 
+    for(int i = 1; i <= n; i++) {
+        cout << mp[i] << " ";
+    }
+ 
+    return 0;
+}
