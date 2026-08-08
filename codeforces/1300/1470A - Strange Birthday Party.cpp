eee57f95f9f49d413430a73e9a5d759f95b8817c
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 18:50:24 +0530

    Add Strange Birthday Party (1300)

---
+++ codeforces/1300/1470A - Strange Birthday Party.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> k(n);
+    vector<ll> c(m);
+
+    for (int i = 0; i < n; i++)
+        cin >> k[i];
+
+    for (int i = 0; i < m; i++)
+        cin >> c[i];
+
+    sort(rall(k));
+
+    ll ans = 0;
+    int gift = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (gift < m && gift < k[i]) {
+            ans += c[gift];
+            gift++;
+        } else {
+            ans += c[k[i] - 1];
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
