c6e1129f666642e7501479e344e705afc77a96c3
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 18:51:05 +0530

    Add K-th Beautiful String (1300)

---
+++ codeforces/1300/1328B - K-th Beautiful String.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n;
+    ll k;
+    cin >> n >> k;
+
+    string ans(n, 'a');
+
+    for (int i = n - 2; i >= 0; i--) {
+        ll cnt = n - i - 1;
+
+        if (k > cnt) {
+            k -= cnt;
+        } else {
+            ans[i] = 'b';
+            ans[n - k] = 'b';
+            break;
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
