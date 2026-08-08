5bed3fa6e4ad28ca9e2d5782ab2466d6b5b16cd7
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:53:09 +0530

    Add Bit++ (800)

---
+++ codeforces/800/282A - Bit++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int x = 0;
+    string s;
+
+    while (n--) {
+        cin >> s;
+
+        if (s == "++X" || s == "X++")
+            x++;
+        else
+            x--;
+    }
+
+    cout << x;
+    return 0;
+}
