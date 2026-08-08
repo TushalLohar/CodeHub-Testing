40ccd3811f8cbbbb51893ccfddc8aa3e04244d53
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 18:51:31 +0530

    Add Romantic Glasses (1300)

---
+++ codeforces/1300/1915E - Romantic Glasses.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    ll prefix = 0;
+    set<ll> st;
+    st.insert(0);
+
+    bool ok = false;
+
+    for (int i = 1; i <= n; i++) {
+        ll x;
+        cin >> x;
+
+        if (i % 2)
+            prefix += x;
+        else
+            prefix -= x;
+
+        if (st.count(prefix))
+            ok = true;
+
+        st.insert(prefix);
+    }
+
+    cout << (ok ? "YES" : "NO") << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
