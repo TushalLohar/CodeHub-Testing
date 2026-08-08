0ae4cf8a015a2ee8ad4abefbc6dd7e48c5aeeaa3
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:45:18 +0530

    Add Perfect Root (800)

---
+++ codeforces/800/2185A - Perfect Root.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long x;
+        cin>>x;
+        for(long long i=1; i<=x;i++){
+            cout<<i<<" ";
+
+        }
+        cout<<endl;
+    }
+}
