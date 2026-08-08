ed36bcf2b98e09bf705c7c21ea0490c4a94b6409
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:12:07 +0530

    Add Turtle and Piggy Are Playing a Game (800)

---
+++ codeforces/800/1981A - Turtle and Piggy Are Playing a Game.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--) {
+        long long l, r;
+        cin >> l >> r;
+ 
+        cout << (int)log2(r) << "\n";
+    }
+}
