c0322fe74d0106e68ebe042081f0fcac9744b128
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:45:45 +0530

    Add Odd One Out (800)

---
+++ codeforces/800/1915A - Odd One Out.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (x == y) cout << z << endl;
+        else if (x == z) cout << y << endl;
+        else cout << x << endl;
+    }
+    return 0;
+}
