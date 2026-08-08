88b91b428a87aa23847dc73db8d6a8de1de27b35
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:50:09 +0530

    Add Word (800)

---
+++ codeforces/800/59A - Word.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+
+    int lower=0;
+    int upper=0;
+    for(int i=0; i<s.size(); i++){
+        if('a'<=s[i] && s[i]<='z'){
+            lower++;
+        }
+        else upper++;
+    }
+
+    if(lower<upper){
+        for(int i=0; i<s.size(); i++){
+            s[i]=toupper(s[i]);
+        }
+        cout<<s;
+    }
+    else {
+        for(int i=0; i<s.size(); i++){
+            s[i]=tolower(s[i]);
+        }
+        cout<<s;
+    }
+}
