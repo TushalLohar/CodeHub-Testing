2922b39512921c80af34b260fcc581d6dff9d659
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:47:24 +0530

    Add Division- (800)

---
+++ codeforces/800/1669A - Division-.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int rating;
+        cin>>rating;
+        if(rating<=1399){
+            cout<<"Division 4"<<endl;
+        }
+        else if(1400<=rating && rating<=1599){
+            cout<<"Division 3"<<endl;
+        }
+        else if(1600<=rating && rating<=1899){
+            cout<<"Division 2"<<endl;
+        }
+        else if(rating>=1900){
+            cout<<"Division 1"<<endl;
+
+        }
+    }
+
+}
