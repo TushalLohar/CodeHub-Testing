511d4a8f2cb73c39d28d68a537366c7cda3b2888
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:39:01 +0530

    Add Linear Keyboard (800)

---
+++ codeforces/800/1607A - Linear Keyboard.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--){
+        string keyboard,s;
+        cin>>keyboard>>s;
+        vector<int> pos(26);
+        for(int i = 0; i < 26; i++){
+            pos[keyboard[i] - 'a'] = i;
+        }
+        int ans = 0;
+        for(int i = 1; i < s.length(); i++){
+            ans += abs(pos[s[i] - 'a'] - pos[s[i-1] - 'a']);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
