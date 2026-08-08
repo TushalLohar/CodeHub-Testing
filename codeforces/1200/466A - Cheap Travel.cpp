91a27b8085c88b81009759656343bd8c991c0eec
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:12:51 +0530

    Add Cheap Travel (1200)

---
+++ codeforces/1200/466A - Cheap Travel.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main() {
+    fast_io;
+
+    int n , m ,a ,b;
+    cin>>n>>m>>a>>b;
+
+    int daily = n*a;
+    int mix = (n/m)*b + (n%m)*a;
+    int special = ((n+m-1)/m)*b;
+
+    cout<<min(daily, min( mix , special));
+    
+
+    
+
+   
+
+    return 0;
+}
