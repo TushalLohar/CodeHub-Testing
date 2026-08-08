9446742f0106b31da75b0c1e7e0fd90ca2d37712
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 18:49:34 +0530

    Add Marenol (easy version) (Unrated)

---
+++ codeforces/Unrated/2254C1 - Marenol (easy version).cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n;
+    cin>>n;
+    string x,y;
+    cin>>x>>y;
+    bool ans=false;
+    ll odd_diff=0 , even_diff=0;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            if(x[i]=='1') odd_diff++;
+            if(y[i]=='1') odd_diff--;
+        }
+        else{
+            if(x[i]=='1') even_diff++;
+            if(y[i]=='1') even_diff--;
+        }
+    }
+    if(odd_diff==0 && even_diff==0){
+        ans =true;
+    }
+    if(ans){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t=1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
