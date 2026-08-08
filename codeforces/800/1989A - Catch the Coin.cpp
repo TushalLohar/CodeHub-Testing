a32b7f47b608db604264e8a84ae6e860b911eca8
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:52:53 +0530

    Add Catch the Coin (800)

---
+++ codeforces/800/1989A - Catch the Coin.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long x, y;
+        cin >> x >> y;
+        if (y > -2)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
