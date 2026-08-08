2fd28ef0f87df78e4f367a3f6c8e7a9876bde433
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 18:50:39 +0530

    Add Yet Another Problem About Pairs Satisfying an Inequality (1300)

---
+++ codeforces/1300/1703F - Yet Another Problem About Pairs Satisfying an Inequality.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<ll> a(n + 1);
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    vector<int> valid;
+    ll ans = 0;
+
+    for (int j = 1; j <= n; j++) {
+        if (a[j] < j) {
+            ans += lower_bound(all(valid), (int)a[j]) - valid.begin();
+            valid.pb(j);
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
