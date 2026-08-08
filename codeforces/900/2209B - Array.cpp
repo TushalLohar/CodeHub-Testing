05f3076df2498107fa8f930c006752c08b379ed8
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:30:16 +0530

    Add Array (900)

---
+++ codeforces/900/2209B - Array.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        ll n;
+        cin >> n;
+
+        vector<ll> nums(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> nums[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+
+            int greater = 0, less = 0;
+
+            for (int j = i + 1; j < n; j++)
+
+            {
+                if (nums[j] > nums[i])
+                    greater++;
+                else if (nums[j] < nums[i])
+                    less++;
+            }
+            cout << max(greater, less);
+            if (i < n - 1)
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
