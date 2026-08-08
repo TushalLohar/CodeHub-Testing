407e3700a77e88ae7def52a8b55a982d19c34a0c
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 18:49:22 +0530

    Add Shuffle (1300)

---
+++ codeforces/1300/1366B - Shuffle.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    ll n, x;
+    int m;
+    cin >> n >> x >> m;
+
+    ll L = x, R = x;
+
+    while (m--) {
+        ll l, r;
+        cin >> l >> r;
+
+        if (r < L || l > R)
+            continue;
+
+        L = min(L, l);
+        R = max(R, r);
+    }
+
+    cout << R - L + 1 << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
