3567e124dd594608fbff9e2278b949b05a18bdca
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:16:10 +0530

    Add Vlad and Candies (800)

---
+++ codeforces/800/1660B - Vlad and Candies.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+ 
+    sort(a.begin(), a.end());
+ 
+    if (n == 1)
+    {
+        if (a[0] > 1)
+        {
+            cout << "NO" << endl;
+            continue;
+        }
+        else
+        {
+            cout << "YES" << endl;
+            continue;
+        }
+    }
+ 
+    if (a[n - 1] - a[n - 2] > 1)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+
+    }
+
+    return 0;
+}
