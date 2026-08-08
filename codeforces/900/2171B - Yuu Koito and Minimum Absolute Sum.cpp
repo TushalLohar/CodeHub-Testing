9f1eaf1f179eedbde1772b62a068739cd814a073
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:50:21 +0530

    Add Yuu Koito and Minimum Absolute Sum (900)

---
+++ codeforces/900/2171B - Yuu Koito and Minimum Absolute Sum.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        if (a[0] == -1 && a[n-1] == -1) {
+            a[0] = 0;
+            a[n-1] = 0;
+        } else if (a[0] == -1) {
+              a[0] = a[n-1];
+        } else if (a[n-1] == -1) {
+            a[n-1] = a[0];
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (a[i] == -1) a[i] = 0;
+        }
+
+        long long diff = llabs(a[n - 1] - a[0]);
+        cout << diff << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << a[i];
+            if (i + 1 == n)
+            cout <<endl;   
+            else
+            cout << " ";   
+
+        }
+    }
+
+    return 0;
+}
