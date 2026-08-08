f14f6930ebd9bb0695b7954e6d306df8ef41dc49
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:47:43 +0530

    Add Sublime Sequence (800)

---
+++ codeforces/800/2148A - Sublime Sequence.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int z,n;
+        cin>>z>>n;
+        if(n%2==0){
+            cout<<"0"<<endl;
+        }
+        else{
+            cout<<z<<endl;
+        }
+    }
+ 
+}
