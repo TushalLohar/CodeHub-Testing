70506a7c327c8101a786e1cb8c0c10ff3cc272eb
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:11:58 +0530

    Add Dinner Time (900)

---
+++ codeforces/900/2102A - Dinner Time.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n,m,p,q;
+        cin>>n>>m>>p>>q;
+        if(n%p==0&&m*p!=n*q) 
+            cout<<"NO"<<"\n";
+        else    
+            cout<<"YES"<<"\n";
+    }
+}
