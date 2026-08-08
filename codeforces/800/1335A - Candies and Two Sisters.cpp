8d3185e4211b26b20a968a2956f30db1a9bbce08
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:49:25 +0530

    Add Candies and Two Sisters (800)

---
+++ codeforces/800/1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        // long long count=0;
+       
+        // long long i=1;
+        // while(true){
+        // long long a=n-i;
+        // long long b=n-a;
+           
+        //    if(a>b){
+        //     count++;
+        //    }
+        //    else{
+        //     break;
+        //    }
+        //     i++;
+
+        // }
+        // cout<<count<<endl;
+
+        cout<<(n-1)/2<<endl;
+       
+    }
+}
