e0e280e8b075271b5744ab6e7dea436a29495a7a
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:21:22 +0530

    Add Find-Pivot-Index (array)

---
+++ leetcode/array/724-Find-Pivot-Index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int total=0;
+        for(int x: nums){
+            total+=x;
+        }
+        int left =0;
+        for(int i=0;i<n;i++){
+            if(left==total-left-nums[i]){
+                return i;
+            }
+            left+=nums[i];
+        }
+        return -1;
+        
+
+
+
+        
+    }
+};
