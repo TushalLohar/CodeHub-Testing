9db4669eb577f7498194c9c8826f88067d3be21c
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:29:33 +0530

    Add Mainak and Array (900)

---
+++ codeforces/900/1726A - Mainak and Array.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++)
+            cin >> nums[i];
+
+        ll ans = nums[n - 1] - nums[0];
+
+        for (int i = 0; i < n; i++)
+        {
+            ans = max(ans, (ll)(nums[n - 1] - nums[i]));
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            ans = max(ans, (ll)(nums[i] - nums[0]));
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            ans = max(ans, (ll)(nums[i] - nums[i + 1]));
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
