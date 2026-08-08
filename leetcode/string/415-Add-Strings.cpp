0c5318fd54a9987ae832d30580bf3e0bc9801f20
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:20:17 +0530

    Add Add-Strings (string)

---
+++ leetcode/string/415-Add-Strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        string ans = "";
+        int carry =0;
+
+        while(i>=0 || j>=0 || carry>0){
+            int digit1 = (i>=0)? num1[i]-'0':0;
+            int digit2 = (j>=0)? num2[j]-'0':0;
+
+            int sum = digit1+digit2+carry;
+            ans.push_back((sum%10) +'0');
+            carry = sum/=10;
+
+            i--;
+            j--;
+
+
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+        
+    }
+};
