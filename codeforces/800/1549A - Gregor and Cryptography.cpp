4448c82b712eb02a84da8e0ba6eeac2ba5a788b5
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:24:20 +0530

    Add Gregor and Cryptography (800)

---
+++ codeforces/800/1549A - Gregor and Cryptography.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int P;
+        cin >> P;
+        cout << 2 << " " << P - 1 << "\n";
+    }
+}
