9c4741bab798862641ab6e35b4addab1c4118b54
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 18:54:59 +0530

    Add Binary String Battle (1200)

---
+++ codeforces/1200/2123D - Binary String Battle.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; ++i)
+        if (s[i] == '1')
+            ans++;
+    if (ans <= k || n < 2 * k)
+        cout << "Alice\n";
+    else
+        cout << "Bob\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
