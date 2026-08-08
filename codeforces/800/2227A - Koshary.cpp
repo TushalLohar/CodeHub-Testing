96efcff3061a313e7b880cb8e2e41bf8de2828f6
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-08 22:24:51 +0530

    Add Koshary (800)

---
+++ codeforces/800/2227A - Koshary.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int x, y;
+        cin >> x >> y;
+        if (x % 2 == 1 && y % 2 == 1)
+            cout << "NO"<<endl;
+        else
+            cout << "YES"<<endl;
+    }
+
+    return 0;
+}
