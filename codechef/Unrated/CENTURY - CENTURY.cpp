d141112c020266ffb14594025f8b124b82fda384
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:08:14 +0530

    Add CENTURY (Unrated)

---
+++ codechef/Unrated/CENTURY - CENTURY.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void checkCentury(int runs) {
+        // write your code here
+        if (runs>=100){
+            cout<<"CENTURY";
+        }
+        else{
+            cout<<"NOT CENTURY";
+        }
+    }
+};
