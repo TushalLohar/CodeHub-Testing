d28023d7d0bd2e6d3563f4ac26a2f2261b6ca518
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 04:20:33 +0530

    Add PAYSC (200)

---
+++ codechef/200/PAYSC - PAYSC.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(100 +x*4<=300){
+	    cout<<100+4*x;
+	}
+	else{
+	    cout<<300;
+	}
+	
+
+}
