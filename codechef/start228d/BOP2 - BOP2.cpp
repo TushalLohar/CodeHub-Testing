af4fa913f2040de2a5236a404c122ea3d6e9535d
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 03:29:38 +0530

    Add BOP2 (start228d)

---
+++ codechef/start228d/BOP2 - BOP2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+     int n;
+     cin>>n;
+      set<int> nums;
+
+        for(int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            nums.insert(x);
+        }
+
+        cout << nums.size() << endl;
+
+
+    }
+}
