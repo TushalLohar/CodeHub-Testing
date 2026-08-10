a3b950bf11e4e26653d1f0d053cfa76134e3cf31
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 04:18:15 +0530

    Add Bitwise Equation (1600)

---
+++ codechef/1600/BITEQU - Bitwise Equation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+
+        long long a = (1LL<<36)+(1LL<<37);
+        long long b=(1LL<<36);
+        long long c=(1LL<<35);
+        long long d=(1LL<<36)+(1LL<<35)+n;
+
+        cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+
+
+
+    }
+}
