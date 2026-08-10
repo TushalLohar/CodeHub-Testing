26979593dc610fe3067bbfbde22fee8fdb2100a8
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:11:34 +0530

    Add SELLCOIN (200)

---
+++ codechef/200/SELLCOIN - SELLCOIN.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int A, B;
+    cin >> A >> B;
+
+    int totalMoney = A + 2 * B;
+    cout << totalMoney << endl;
+
+    return 0;
+}
