ee052100f2b32a1f694eded513fdffe2eab1702c
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:09:02 +0530

    Add P1209 (100)

---
+++ codechef/100/P1209 - P1209.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // your code goes here
+    int r;
+    cin >> r;
+    if (r <= 4) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO";
+    }
+}
