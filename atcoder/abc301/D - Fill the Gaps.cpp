8af8c13f108af2e2376849cac5c2e3b9447bad57
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 02:05:24 +0530

    Add Fill the Gaps (abc301)

---
+++ atcoder/abc301/D - Fill the Gaps.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<n-1; i++){
+         cout<<arr[i]<<" ";
+        if (arr[i]<arr[i+1]){
+           
+            int temp = arr[i]+1;
+            while(temp<arr[i+1]){
+                cout<<temp<<" ";
+                temp++;
+            }
+            
+        }
+        else if(arr[i]>arr[i+1]){
+          
+            int temp= arr[i]-1;
+            while(temp>arr[i+1]){
+                cout<<temp<<" ";
+                temp--;
+            }
+           
+        }
+    }
+     cout<<arr[n-1]<<" ";
+}
