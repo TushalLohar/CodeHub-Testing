0195906fe08c7177fa187c88c630bc919ea98119
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 03:26:11 +0530

    Add DISCOUNT7 (start233c)

---
+++ codechef/start233c/DISCOUNT7 - DISCOUNT7.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>=5){
+	    cout<<n*85<<endl;
+	}
+	else{
+	    cout<<n*100<<endl;
+	}
+
+}
