e3eefba2cd13f4c1f62575264129535792b90c1e
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:12:56 +0530

    Add VAD3V2 (Unrated)

---
+++ codechef/Unrated/VAD3V2 - VAD3V2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Assign the value 23 to a
+int a=23;
+int b=20;
+int c=a+b;
+cout<<c;
+    
+    // Assign the value 20 to b
+
+    
+    // Output the sum of a and b
+}
