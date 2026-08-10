50d89f2a813350c6733715c1f6b2c9959e7cc5e6
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 03:29:13 +0530

    Add BOP3 (start228d)

---
+++ codechef/start228d/BOP3 - BOP3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+    cin >> t;
+
+    while(t--) {
+        long long n;
+        cin >> n;
+
+        cout << (n * (n + 2)) / 4 << "\n";
+    }
+
+}
