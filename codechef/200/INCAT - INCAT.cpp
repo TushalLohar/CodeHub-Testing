568908826f8cd40d3f6aa2be7c043766009a7f4c
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:11:09 +0530

    Add INCAT (200)

---
+++ codechef/200/INCAT - INCAT.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+string s;
+cin>>s;
+sort(s.begin(),s.end());
+if(s=="act"){
+    cout<<"yes";
+}
+else{
+    cout<<"no";
+}
+}
