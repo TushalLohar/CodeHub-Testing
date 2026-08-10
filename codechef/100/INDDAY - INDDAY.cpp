8a5698b2f283dde2da4ce96dc9b4253ce2e7a051
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:10:03 +0530

    Add INDDAY (100)

---
+++ codechef/100/INDDAY - INDDAY.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int num;
+cin>>num;
+if(num>15){
+    cout<<-1;
+}
+else{
+int day = 15- num;
+
+cout<<day;
+}
+}
