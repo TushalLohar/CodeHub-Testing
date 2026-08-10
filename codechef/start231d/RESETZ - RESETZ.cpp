419dc9ba1f1d7a17d22f97b1030961521c0b6787
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 03:26:24 +0530

    Add RESETZ (start231d)

---
+++ codechef/start231d/RESETZ - RESETZ.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include<unordered_set>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n;
+        cin>>n;
+
+        vector<int> nums(n);
+        for(int i=0;i<n;i++) cin>>nums[i];
+
+        int maxi =*max_element(all(nums));
+
+        unordered_set<int> seen;
+        bool ans =true;
+        for(int i=0;i<n;i++){
+           if(nums[i]!=maxi){
+            int r=maxi-nums[i];
+            if(seen.count(r)){
+                ans=false;
+                break;
+                
+            }
+            seen.insert(r);
+           }
+        }
+
+
+               cout << (ans ? "Yes" : "No") << "\n";
+
+
+    }
+
+    return 0;
+}
