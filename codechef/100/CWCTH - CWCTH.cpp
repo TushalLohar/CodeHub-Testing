e2a680f72569bbcef3c766a55c3063e1eec14341
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:10:16 +0530

    Add CWCTH (100)

---
+++ codechef/100/CWCTH - CWCTH.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int A;
+	cin>>A;
+	int B;
+	cin>>B;
+	if(B>=3*A){
+	    cout<<"Rain";
+	}
+	else{
+	    cout<<"Dry";
+	}
+
+}
