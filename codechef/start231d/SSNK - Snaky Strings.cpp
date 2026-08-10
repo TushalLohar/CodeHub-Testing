ef0a2dfe6516cfdab7909fd51ae7d32bda91f181
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 03:28:08 +0530

    Add Snaky Strings (start231d)

---
+++ codechef/start231d/SSNK - Snaky Strings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string x;
+	cin>>x;
+	if (x[0] == 's' || x[3] == 's') {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+
+}
