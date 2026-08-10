2b408e8c1cc5c4c15569f03caa96fa3bfced87d9
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:07:19 +0530

    Add SCORING (500)

---
+++ codechef/500/SCORING - SCORING.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x , y;
+	    cin>>x>>y;
+	    int bob = (y-x)/2;
+	    int alice = bob+x;
+	    
+	    cout<<alice<<" "<<bob<<endl;
+	}
+
+}
