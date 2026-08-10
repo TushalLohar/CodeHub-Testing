844971f8e84f47ff1991d3d6668e9c6dd9ddcb2f
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 05:05:45 +0530

    Add TOYS (200)

---
+++ codechef/200/TOYS - TOYS.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n , m;
+    cin>>n>>m;
+    if(n>m){
+        cout<<n-m;
+    }
+    else if(n==m){
+        cout<<0;
+    }
+    else if(n<m){
+        cout<<0;
+    }
+    
+
+}
