5f43b3889708a06d5c0f50fdd6b1cac34220de07
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 03:26:04 +0530

    Add Movie (start233c)

---
+++ codechef/start233c/MOVIE7 - Movie.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// ---------- FAST IO ----------
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// ---------- TYPES ----------
+#define endl '\n'
+#define ll long long
+#define ld long double
+
+// ---------- SHORTCUTS ----------
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// ---------- CONSTANTS ----------
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+int main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n,m,a,b,c;
+        cin>>n>>m>>a>>b>>c;
+
+        ll combos = min(n,m);
+        ll ans= combos*c + (n-combos)*a + (m-combos)*b;
+
+
+        cout<<ans<<endl;
+
+        
+
+    }
+
+    return 0;
+}
