1ab0cea0c64c8bd5ffab700ae493aeb0a7d5bf01
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 04:09:54 +0530

    Add Qualified or Not (200)

---
+++ codechef/200/QNOT - Qualified or Not.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n , x , y;
+	cin>>n>>x>>y;
+	if(n>=2*x && n>=2*y){
+	    cout<<"YES"<<endl;
+	    
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
