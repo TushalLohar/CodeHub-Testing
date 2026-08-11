d0786f4453298fa8f83a77d8bfeca3e82297ca5d
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:34 +0530

    Add C++ if-else (Decision Making) (Basic)

---
+++ geeksforgeeks/Basic/C++ if-else (Decision Making).cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: C++ if-else (Decision Making) (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/c-if-else-decision-making4138/1
+ * Solved: 2025-06-28 13:08:11
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
