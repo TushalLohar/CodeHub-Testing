21f18114c6eee47c7e97aefc98c4a7dfa2bec522
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:21 +0530

    Add Armstrong Numbers (Easy)

---
+++ geeksforgeeks/Easy/Armstrong Numbers.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Armstrong Numbers (Easy)
+ * URL: https://www.geeksforgeeks.org/problems/armstrong-numbers2727/1
+ * Solved: 2025-07-27 02:52:10
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
