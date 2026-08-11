9ad7d716429e002a57c0369d6d17b6b0ce7cfe49
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:31 +0530

    Add The New Line (Easy)

---
+++ geeksforgeeks/Easy/The New Line.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: The New Line (Easy)
+ * URL: https://www.geeksforgeeks.org/problems/the-new-line/1
+ * Solved: 2025-06-28 13:13:25
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
