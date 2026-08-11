ea2b6e43f148fff6e7da35133969db6d284ae2a3
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:36 +0530

    Add Decision Making (Basic)

---
+++ geeksforgeeks/Basic/Decision Making.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Decision Making (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/decision-making/1
+ * Solved: 2025-06-28 13:01:04
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
