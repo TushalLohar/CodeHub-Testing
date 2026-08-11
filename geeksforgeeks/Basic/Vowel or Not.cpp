6a0affda017a0a24253f3bb659681f7640c53e71
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:23 +0530

    Add Vowel or Not (Basic)

---
+++ geeksforgeeks/Basic/Vowel or Not.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Vowel or Not (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/vowel-or-not0831/1
+ * Solved: 2025-06-28 13:38:56
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
