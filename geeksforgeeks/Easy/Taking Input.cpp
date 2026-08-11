d50754bd8859e2d453cf4019e9eb613e9e2db534
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:26 +0530

    Add Taking Input (Easy)

---
+++ geeksforgeeks/Easy/Taking Input.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Taking Input (Easy)
+ * URL: https://www.geeksforgeeks.org/problems/taking-input/1
+ * Solved: 2025-06-28 13:25:50
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
