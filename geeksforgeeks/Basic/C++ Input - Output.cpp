5fdf2a9b92efd16d4ec78fd9710dbbd5056f7918
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:27 +0530

    Add C++ Input - Output (Basic)

---
+++ geeksforgeeks/Basic/C++ Input - Output.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: C++ Input / Output (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/c-input-output2432/1
+ * Solved: 2025-06-28 13:22:38
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
