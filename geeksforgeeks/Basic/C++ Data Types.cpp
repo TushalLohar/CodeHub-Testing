495abdb424ac89e56ffa24d1377d1049c5c64195
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:38 +0530

    Add C++ Data Types (Basic)

---
+++ geeksforgeeks/Basic/C++ Data Types.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: C++ Data Types (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/c-data-types1523/1
+ * Solved: 2025-06-18 23:56:27
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
