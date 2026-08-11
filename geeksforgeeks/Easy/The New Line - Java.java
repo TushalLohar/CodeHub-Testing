4f5e1ef9062f41251457893c54b1e1113bfe6406
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:39 +0530

    Add The New Line - Java (Easy)

---
+++ geeksforgeeks/Easy/The New Line - Java.java
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: The New Line - Java (Easy)
+ * URL: https://www.geeksforgeeks.org/problems/the-new-line-java/1
+ * Solved: 2024-07-16 14:52:14
+ * Language: java
+ *
+ * Solution synced from GeeksforGeeks
+ */
