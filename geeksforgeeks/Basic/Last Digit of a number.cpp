e344d8de09b196a5d24e229a9939334dfbc6109d
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:19 +0530

    Add Last Digit of a number (Basic)

---
+++ geeksforgeeks/Basic/Last Digit of a number.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Last Digit of a number (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/last-digit-of-a-number--145429/1
+ * Solved: 2025-07-27 11:43:05
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
