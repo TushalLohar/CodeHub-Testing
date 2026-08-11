416b67964bc062f440d833b18552160d82899aff
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:11:16 +0530

    Update Smallest-Missing-Integer-Greater-Than-Sequential-Prefix-Sum (hash-table)

--- leetcode/hash-table/2996-Smallest-Missing-Integer-Greater-Than-Sequential-Prefix-Sum.cpp
+++ leetcode/hash-table/2996-Smallest-Missing-Integer-Greater-Than-Sequential-Prefix-Sum.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int missingInteger(vector<int>& nums) {
         unordered_set<int> st(nums.begin(), nums.end());
-
         int sum = nums[0];
-
         int i = 1;
         while (i < nums.size() && nums[i] == nums[i - 1] + 1) {
             sum += nums[i];
             i++;
         }
-
         while (st.count(sum))
             sum++;
 
