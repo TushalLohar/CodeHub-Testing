4fc83bce544dc4c37f758bd25d82e67ae8d0776e
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:30 +0530

    Add Learn to Comment (Easy)

---
+++ geeksforgeeks/Easy/Learn to Comment.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Learn to Comment (Easy)
+ * URL: https://www.geeksforgeeks.org/problems/learn-to-comment/1
+ * Solved: 2025-06-28 13:14:31
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
