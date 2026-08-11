e4e90630981b2c42f0b45de2df318acb697bd835
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:16 +0530

    Add Pyramid Pattern (Basic)

---
+++ geeksforgeeks/Basic/Pyramid Pattern.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Pyramid Pattern (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/pyramid-patterns/1
+ * Solved: 2025-12-14 11:20:03
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
