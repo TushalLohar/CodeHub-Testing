e1ac5448332fd6c5402ec6315c724da0b1ae7ba7
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:37 +0530

    Add Find if +ve, -ve or 0 (Basic)

---
+++ geeksforgeeks/Basic/Find if +ve, -ve or 0.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Find if +ve, -ve or 0 (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/find-if-ve-ve-or-0--141631/1
+ * Solved: 2025-06-26 10:40:46
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
