5ad89e6d92a8b3f0cf7c2bc2918f325b0fc1a147
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:28 +0530

    Add Data Types (Easy)

---
+++ geeksforgeeks/Easy/Data Types.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Data Types (Easy)
+ * URL: https://www.geeksforgeeks.org/problems/data-types/1
+ * Solved: 2025-06-28 13:21:08
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
