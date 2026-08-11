95f2082553d3124572b81fe891a7cad985942b16
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:24 +0530

    Add Start Coding (Basic)

---
+++ geeksforgeeks/Basic/Start Coding.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Start Coding (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/start-coding--171550/1
+ * Solved: 2025-06-28 13:27:08
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
