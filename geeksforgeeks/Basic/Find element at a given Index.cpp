2531e82aabaafc3f62b74b3b7914c5e171fb7225
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:20 +0530

    Add Find element at a given Index (Basic)

---
+++ geeksforgeeks/Basic/Find element at a given Index.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: Find element at a given Index (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/c-array-print-an-element-set-25933/1
+ * Solved: 2025-07-27 02:57:18
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
