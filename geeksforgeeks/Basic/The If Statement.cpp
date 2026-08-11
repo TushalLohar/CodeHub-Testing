3da471d8aa8473d6b5dd65e2fce7b8627508294f
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-11 22:45:22 +0530

    Add The If Statement (Basic)

---
+++ geeksforgeeks/Basic/The If Statement.cpp
@@ -0,0 +1,9 @@
+/**
+ * User: GeeksforGeeks
+ * Problem: The If Statement (Basic)
+ * URL: https://www.geeksforgeeks.org/problems/the-if-statement--113256/1
+ * Solved: 2025-06-28 13:54:22
+ * Language: cpp
+ *
+ * Solution synced from GeeksforGeeks
+ */
